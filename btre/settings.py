2638121b5e711fe7ec7164f033e2cec25f339882
Yongzhe Zhang <jcddyongzhe@gmail.com>
2020-07-04 14:08:32 +0800

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -142,12 +142,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 25
-EMAIL_HOST_USER = 'jcddyongzhe@gmail.com'
-EMAIL_HOST_PASSWORD = '20Jianchidaodi'
-EMAIL_USE_TLS = True
 
 try:
      from .local_settings import *
